1b9d64691136ae0f34075ec57432399fce979111
Briscese <gabrielbrosig@gmail.com>
2025-03-29 14:26:00 -0300

    correção do requirements.txt

--- src/main.py
+++ src/main.py
@@ -12,4 +12,4 @@ async def root():
     return {"message": "API de Pré-processamento PLN está rodando!"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5001, reload=True)
