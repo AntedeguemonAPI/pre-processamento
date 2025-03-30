f7e951d3c80fb1fca5caa180d79ccd88cdf96462
Jean Faria <111452998+jeejinf@users.noreply.github.com>
2025-03-30 04:01:59 -0300

    fix: adicionando CORS para a porta 3001

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes import files
 import uvicorn
 
 app = FastAPI()
+
+# Configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3001"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Incluindo as rotas de upload/download
 app.include_router(files.router, prefix="/files", tags=["Arquivos"])
