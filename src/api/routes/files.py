a86bc1569f0f159255b38604da3036e03aae471b
Miguel <doxito007@gmail.com>
2025-05-10 15:26:12 -0300

    feat: Adicionando mais informações para o processamento

--- src/api/routes/files.py
+++ src/api/routes/files.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import requests
 import json
+import time
+import httpx
 import asyncio
 from utils.file_utils import load_csv
 from preprocess.process_pipeline import preprocess_text_column
@@ -14,7 +16,8 @@ PROCESSED_DIR = "./data/processed/"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 os.makedirs(PROCESSED_DIR, exist_ok=True)
 
-ID_SERVICE_URL = "http://banco-de-dados:5003"
+ID_SERVICE_URL = "http://localhost:5003"
+ID_SERVICE_URL_PROCESSAMENTO = "http://localhost:5004"
 
 async def process_pipeline(file_path: str, id_gerado: int):
     try:
@@ -30,6 +33,17 @@ async def process_pipeline(file_path: str, id_gerado: int):
     except Exception as e:
         print(f"Erro ao processar o arquivo: {str(e)}")
 
+async def background_pipeline(file_path: str, id_gerado: int):
+    try:
+        await process_pipeline(file_path, id_gerado)
+        await send_tokenization_to_api(file_path="./data/processed/dataset_processado.csv", id_gerado=id_gerado)
+        
+        async with httpx.AsyncClient() as client:
+            response = await client.post(f"http://processamento:5004/indexar/{id_gerado}")
+            print(f"Indexação finalizada com status: {response.status_code}")
+    except Exception as e:
+        print(f"Erro na pipeline em background: {e}")
+
 async def send_tokenization_to_api(id_gerado, file_path):
 
     df = load_csv(file_path)
@@ -40,18 +54,33 @@ async def send_tokenization_to_api(id_gerado, file_path):
     for index, row in df.iterrows():        
         resultado_json = {
             "ID_geral": id_gerado,
-            "Descrição_tokens_filtered": row["Descrição_tokens_filtered"],
-            "Descrição_tokens": row["Descrição_tokens"],
-            "Solução - Solução": row["Solução - Solução"],
-            "Descrição": row["Descrição"],
-            "Data de abertura": row["Data de abertura"],
-            "Data de fechamento": row["Data de fechamento"]
+            "Título": row.get("Título") or row.get("Nome do projeto"),
+            "Tipo": row.get("Tipo"),
+            "Descrição": row.get("Descrição"),
+            "Solução - Solução": row.get("Solução - Solução"),
+            "Descrição_tokens_filtered": row.get("Descrição_tokens_filtered"),
+            "Descrição_tokens": row.get("Descrição_tokens"),
+            "Localização": row.get("Localização"),
+            "Elementos associados": row.get("Elementos associados"),
+            "Data de abertura": row.get("Data de abertura") or row.get("Criado"),
+            "Data de fechamento": row.get("Data de fechamento") or row.get("Resolvido"),
+            "Tempo interno para atendimento excedido": row.get("Tempo interno para atendimento excedido"),
+            "Tempo para atendimento": row.get("Tempo para atendimento"),
+            "Limite do tempo interno de atendimento": row.get("Tempo interno para atendimento"),
+            "ID_chamado": row.get("ID")
         }
 
+        
+        print("Enviando dados para o serviço de tokenização...")
+        print("Json enviado:", resultado_json)
+        tempo_inicial = time.time()
         response = requests.post(
             f"{ID_SERVICE_URL}/texto_limpo",
             json=resultado_json
         )
+        print("Resposta do serviço de tokenização:", response.status_code)
+        print("Tempo de resposta:", time.time() - tempo_inicial)
+
 
         print(f"Enviado ID {id_gerado}: {response.status_code}")
 
@@ -76,20 +105,17 @@ async def upload_csv(file: UploadFile = File(...)):
     file_path = os.path.join(UPLOAD_DIR, file.filename)
     with open(file_path, "wb") as buffer:
         buffer.write(await file.read())
-    
-    asyncio.create_task(process_pipeline(file_path, id_gerado))
-    
-    asyncio.create_task(send_tokenization_to_api(file_path="./data/processed/dataset_processado.csv" ,id_gerado=id_gerado))
-    
-    asyncio.create_task(requests.post(f"http://processamento:5004/indexar/{id_gerado}"))
-    
+
+    # Chama pipeline completa em background
+    asyncio.create_task(background_pipeline(file_path, id_gerado))
+
     try:
         final_response = requests.get(f"{ID_SERVICE_URL}/ids/{id_gerado}")
         final_response.raise_for_status()
-        
         return final_response.json()
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Erro ao buscar dados do ID gerado: {str(e)}")
+
 
 
 @router.get("/download_csv/{filename}")
